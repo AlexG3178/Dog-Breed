478620b4dee42be2599409d46961025c9e6ec87d
AlexG3178 <alexg3178@gmail.com>
2024-08-29 14:29:50 +0200

    EfficientNet+SGD

--- dog_breed_cnn.py
+++ dog_breed_cnn.py
@@ -19,10 +19,15 @@ print('CUDA Available =', torch.cuda.is_available())
 print(torch.cuda.get_device_name(0))
 
 
-n_epochs = 50
+n_epochs = 10
 batch_size = 32
 num_workers = 0
+# optimizer = torch.optim.Adam
+# optimizer = torch.optim.SGD(lr=0.01, momentum=0.9)
+optimizer = torch.optim.SGD
 
+
+#region Dataset & Transforms
 def rename(name):
     new_name = name.split('.')[1]
     new_name = new_name.replace('_',' ')
@@ -30,7 +35,6 @@ def rename(name):
 
 for name in dataset.classes:
     breeds.append(rename(name))
-    
 
 train_ds, test_dev_ds = train_test_split(dataset, test_size=0.4, train_size=0.6, random_state=34)
 val_ds, test_ds = train_test_split(test_dev_ds, test_size=0.5, train_size=0.5, random_state=34)
@@ -94,6 +98,11 @@ train_dl = DataLoader(train_dataset, batch_size, shuffle=True, num_workers=num_w
 val_dl = DataLoader(val_dataset, batch_size*2, num_workers=num_workers, pin_memory=True)
 test_dl = DataLoader(test_dataset, batch_size*2, num_workers=num_workers, pin_memory=True)
 
+#endregion
+
+
+
+#region: Model
 
 def accuracy(outputs, labels):
     _, preds = torch.max(outputs, dim=1)
@@ -122,53 +131,71 @@ class ImageClassificationBase(nn.Module):
 
     def epoch_end(self, epoch, result):
         print(f"Epoch [{epoch+1}], train_loss: {result['train_loss']:.4f}, val_loss: {result['val_loss']:.4f}, val_acc: {result['val_acc']:.4f}")
+        
 
-# class DogBreedClassificationCNN(ImageClassificationBase):
-#     def __init__(self):
-#         super().__init__()
-#         self.network = nn.Sequential(
-#             nn.Conv2d(3, 32, kernel_size=3, padding=1),
-#             nn.BatchNorm2d(32),
-#             nn.ReLU(),
-#             nn.Conv2d(32, 64, kernel_size=3, padding=1),
-#             nn.BatchNorm2d(64),
-#             nn.ReLU(),
-#             nn.MaxPool2d(2, 2),
 
-#             nn.Conv2d(64, 128, kernel_size=3, padding=1),
-#             nn.BatchNorm2d(128),
-#             nn.ReLU(),
-#             nn.Conv2d(128, 256, kernel_size=3, padding=1),
-#             nn.BatchNorm2d(256),
-#             nn.ReLU(),
-#             nn.MaxPool2d(2, 2),
+class DogBreedClassificationCNN(ImageClassificationBase):
+    def __init__(self):
+        super().__init__()
+        self.network = nn.Sequential(
+            
+            # Convolutional Layer 1
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),  # Reduces 224x224 -> 112x112
+            
+            # Convolutional Layer 2
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),  # Reduces 112x112 -> 56x56
 
-#             nn.Conv2d(256, 512, kernel_size=3, padding=1),
-#             nn.BatchNorm2d(512),
-#             nn.ReLU(),
-#             nn.Conv2d(512, 512, kernel_size=3, padding=1),
-#             nn.BatchNorm2d(512),
-#             nn.ReLU(),
-#             nn.MaxPool2d(2, 2),
+            # Convolutional Layer 3
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),  # Reduces 56x56 -> 28x28
 
-#             nn.Conv2d(512, 512, kernel_size=3, padding=1),
-#             nn.BatchNorm2d(512),
-#             nn.ReLU(),
-#             nn.MaxPool2d(2, 2),
+            # Convolutional Layer 4
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),  # Reduces 28x28 -> 14x14
 
-#             nn.AdaptiveAvgPool2d(1),
-#             nn.Flatten(),
-#             nn.Dropout(0.5),
-#             nn.Linear(512, len(breeds)),
-#             nn.LogSoftmax(dim=1)
-#         )
+            # Flatten Layer
+            nn.Flatten(),
 
-#     def forward(self, xb):
-#         return self.network(xb)
+            # Fully Connected Layer 1
+            nn.Dropout(0.5),
+            nn.Linear(256*14*14, 512),
+            nn.ReLU(),
 
-# model = DogBreedClassificationCNN()
+            # Fully Connected Layer 2 (Output Layer)
+            nn.Dropout(0.5),
+            nn.Linear(512, len(breeds)),
+            nn.LogSoftmax(dim=1)
+        )
 
-class DogBreedClassificationPretrained(ImageClassificationBase):
+    def forward(self, xb):
+        return self.network(xb)
+
+
+class DogBreedClassificationEfficientNet(ImageClassificationBase):
+    def __init__(self):
+        super().__init__()
+        self.network = models.efficientnet_b0(weights='DEFAULT')
+        num_ftrs = self.network.classifier[1].in_features
+        self.network.classifier = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(num_ftrs, len(breeds)),
+            nn.LogSoftmax(dim=1)
+        )
+    def forward(self, xb):
+        return self.network(xb)
+    
+
+class DogBreedClassificationResNet(ImageClassificationBase):
     def __init__(self):
         super().__init__()
         self.network = models.resnet50(weights='DEFAULT')
@@ -178,14 +205,40 @@ class DogBreedClassificationPretrained(ImageClassificationBase):
             nn.Linear(num_ftrs, len(breeds)),
             nn.LogSoftmax(dim=1)
         )
-    
     def forward(self, xb):
         return self.network(xb)
 
-# Model initialization
-model = DogBreedClassificationPretrained()
 
-                                                # TO GPU
+class DogBreedClassificationVGG(ImageClassificationBase):
+    def __init__(self):
+        super().__init__()
+        self.network = models.vgg16(weights='DEFAULT')
+        num_ftrs = self.network.classifier[6].in_features
+        self.network.classifier[6] = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(num_ftrs, len(breeds)),
+            nn.LogSoftmax(dim=1)
+        )
+    def forward(self, xb):
+        return self.network(xb)
+    
+#endregion
+
+# model = DogBreedClassificationCNN()
+# Epoch [10], train_loss: 4.8556, val_loss: 4.8793, val_acc: 0.0087
+
+model = DogBreedClassificationEfficientNet()
+# Epoch [10], train_loss: 0.5459, val_loss: 0.8211, val_acc: 0.7502
+
+# model = DogBreedClassificationResNet()
+# Epoch [10], train_loss: 1.0750, val_loss: 1.0798, val_acc: 0.6796
+
+# model = DogBreedClassificationVGG()
+# Epoch [10], train_loss: 4.8558, val_loss: 4.8786, val_acc: 0.0104
+
+
+#region Device Configuration
+    
 def get_default_device():
     if torch.cuda.is_available():
         return torch.device('cuda')
@@ -210,24 +263,29 @@ class DeviceDataLoader:
     def __iter__(self):
         for batch in self.dl:
             yield to_device(batch, self.device)
-            # torch.cuda.empty_cache()
-            
+            torch.cuda.empty_cache()
+
+#endregion
 
 
-                                                # Training     
+
+#region Training
+                                              
+'''
+When you decorate a function with @torch.no_grad(), all the operations performed inside that function will not track gradients
+'''
 @torch.no_grad()
 def evaluate(model, val_loader):
     model.eval()
     outputs = [model.validation_step(batch) for batch in val_loader]
     return model.validation_epoch_end(outputs)
 
-def fit_one_cycle(epochs, max_lr, model, train_loader, val_loader, 
-                  weight_decay=0, grad_clip=None, opt_func=torch.optim.Adam):
+def fit(epochs, max_lr, model, train_loader, val_loader, weight_decay=0, grad_clip=None, optimizer=optimizer):
+    
     torch.cuda.empty_cache()
     history = []
-    optimizer = opt_func(model.parameters(), max_lr, weight_decay=weight_decay)
-    sched = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr, epochs=epochs, 
-                                                steps_per_epoch=len(train_loader))
+    optimizer = optimizer(model.parameters(), max_lr, weight_decay=weight_decay)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr, epochs=epochs, steps_per_epoch=len(train_loader))
     
     for epoch in range(epochs):
         model.train()
@@ -243,8 +301,8 @@ def fit_one_cycle(epochs, max_lr, model, train_loader, val_loader,
             
             optimizer.step()
             optimizer.zero_grad()
-            sched.step()
-            lrs.append(sched.get_last_lr()[0])
+            scheduler.step()
+            lrs.append(scheduler.get_last_lr()[0])
         
         result = evaluate(model, val_loader)
         result['train_loss'] = torch.stack(train_losses).mean().item()
@@ -254,7 +312,14 @@ def fit_one_cycle(epochs, max_lr, model, train_loader, val_loader,
         
     return history
 
+#endregion
+
+
+
+#region  Main function
+
 if __name__ == "__main__":
+    
     # Device configuration
     device = get_default_device()
     print(f'default device: {device}')
@@ -265,7 +330,7 @@ if __name__ == "__main__":
     to_device(model, device)
     
     # Training
-    history = fit_one_cycle(n_epochs, 0.01, model, train_dl, val_dl, weight_decay=1e-4, grad_clip=0.1)
+    history = fit(n_epochs, 0.01, model, train_dl, val_dl, weight_decay=1e-4, grad_clip=0.1)
     
     # Plotting the training history
     epochs = range(len(history))
@@ -305,4 +370,6 @@ if __name__ == "__main__":
         plt.show()
 
     # Test a prediction
-    predict_single(*test_dataset[4])
+    predict_single(*test_dataset[8])
+    
+    #endregion
